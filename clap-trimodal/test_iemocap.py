b2596fe6a51200431c241af6078acb5d6e1751f9
wpartycja <w512patrycja@gmail.com>
2025-06-06 13:47:23 +0200

    updated iemocap testing

--- clap-trimodal/test_iemocap.py
+++ clap-trimodal/test_iemocap.py
@@ -1,31 +1,35 @@
 import torch
 import random
 import matplotlib.pyplot as plt
+from collections import Counter
 
 from datascripts.iemocap import IEMOCAPDataset
 
 def main():
-    # Toggle this to True/False to test both modes
     use_preprocessed = True
 
     dataset = IEMOCAPDataset(
-        data_dir="data/processed/iemocap" if use_preprocessed else "data/raw/iemocap",
+        data_dir="data/processed_wav/iemocap" if use_preprocessed else "data/raw/iemocap",
         split="train",
         cache_path="cache/iemocap_cache.pkl",
         use_preprocessed_audio=use_preprocessed,
+        max_audio_length=6,
         sample_rate=16000,
-        max_length=5
     )
 
     print(f"Dataset size: {len(dataset)} samples")
-     
-    # Pick a few random samples
-    for i in random.sample(range(len(dataset)), 3):
-        waveform, label, transcript = dataset[i]
-        print(f"\nSample {i}")
-        print(f"Label: {label}")
-        print(f"Transcript: {transcript}")
-        print(f"Waveform shape: {waveform.shape}")
-        
+
+    # Count class occurrences
+    label_counter = Counter()
+    for _, label, _ in dataset:
+        label_counter[label] += 1
+
+    # Print utterance count per class
+    print("\nUtterance count per class:")
+    for label, count in sorted(label_counter.items()):
+        print(f"{label}: {count}")
+
+
+
 if __name__ == "__main__":
     main()
